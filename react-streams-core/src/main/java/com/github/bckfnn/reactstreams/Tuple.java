293817702bfcc0ec734b66d90c818d4b26d46087
bckfnn <bckfnn@gmail.com>
2014-08-22 15:14:20 +0200

    javadoc.

--- react-streams-core/src/main/java/com/github/bckfnn/reactstreams/Tuple.java
+++ react-streams-core/src/main/java/com/github/bckfnn/reactstreams/Tuple.java
@@ -13,64 +13,87 @@
  */
 package com.github.bckfnn.reactstreams;
 
+/**
+ * A simple two value tuple.
+ *
+ * @param <I1> type of the first value.
+ * @param <I2> type of the second value.
+ */
 public class Tuple<I1, I2> {
-	private I1 value1;
-	private I2 value2;
-	
-	public Tuple(I1 value1, I2 value2) {
-		this.value1 = value1;
-		this.value2 = value2;
-	}
-	
-	public I1 getValue1() {
-		return value1;
-	}
+    private I1 value1;
+    private I2 value2;
 
-	public I2 getValue2() {
-		return value2;
-	}
+    /**
+     * Construct a tuple with two values.
+     * @param value1 the first value.
+     * @param value2 the second value.
+     */
+    public Tuple(I1 value1, I2 value2) {
+        this.value1 = value1;
+        this.value2 = value2;
+    }
 
-	public I1 left() {
-		return getValue1();
-	}
+    /**
+     * @return the first value.
+     */
+    public I1 getValue1() {
+        return value1;
+    }
 
-	public I2 right() {
-		return getValue2();
-	}
+    /**
+     * @return the second value.
+     */
+    public I2 getValue2() {
+        return value2;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((value1 == null) ? 0 : value1.hashCode());
-		result = prime * result + ((value2 == null) ? 0 : value2.hashCode());
-		return result;
-	}
+    /**
+     * @return the first value.
+     */
+    public I1 left() {
+        return getValue1();
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Tuple<?, ?> other = (Tuple<?, ?>) obj;
-		if (value1 == null) {
-			if (other.value1 != null)
-				return false;
-		} else if (!value1.equals(other.value1))
-			return false;
-		if (value2 == null) {
-			if (other.value2 != null)
-				return false;
-		} else if (!value2.equals(other.value2))
-			return false;
-		return true;
-	}
+    /**
+     * @return the second value.
+     */
+    public I2 right() {
+        return getValue2();
+    }
 
-	@Override
-	public String toString() {
-		return "Tuple[" + value1 + ", " + value2 + "]";
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((value1 == null) ? 0 : value1.hashCode());
+        result = prime * result + ((value2 == null) ? 0 : value2.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Tuple<?, ?> other = (Tuple<?, ?>) obj;
+        if (value1 == null) {
+            if (other.value1 != null)
+                return false;
+        } else if (!value1.equals(other.value1))
+            return false;
+        if (value2 == null) {
+            if (other.value2 != null)
+                return false;
+        } else if (!value2.equals(other.value2))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Tuple[" + value1 + ", " + value2 + "]";
+    }
 }
