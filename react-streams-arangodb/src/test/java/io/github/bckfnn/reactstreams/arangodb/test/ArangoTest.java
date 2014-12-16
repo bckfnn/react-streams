2780d4dd5eb3150d9da6fad098af04b3c242a9a2
bckfnn <bock.finn@gmail.com>
2014-12-16 07:25:48 +0100

    added license.

--- react-streams-arangodb/src/test/java/io/github/bckfnn/reactstreams/arangodb/test/ArangoTest.java
+++ react-streams-arangodb/src/test/java/io/github/bckfnn/reactstreams/arangodb/test/ArangoTest.java
@@ -1,3 +1,16 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.github.bckfnn.reactstreams.arangodb.test;
 
 import io.github.bckfnn.reactstreams.Stream;
@@ -10,8 +23,6 @@ import org.junit.Test;
 import org.vertx.java.core.json.JsonObject;
 import org.vertx.testtools.TestVerticle;
 import org.vertx.testtools.VertxAssert;
-
-
 
 public class ArangoTest extends TestVerticle {
 
