f163528bcfe50b1e331d21a0509cc4a312ff6067
bckfnn <bock.finn@gmail.com>
2014-12-18 19:23:37 +0100

    added license.

--- react-streams-core/src/test/java/io/github/bckfnn/reactstreams/test/CountPublisherTest.java
+++ react-streams-core/src/test/java/io/github/bckfnn/reactstreams/test/CountPublisherTest.java
@@ -1,3 +1,16 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.github.bckfnn.reactstreams.test;
 
 import io.github.bckfnn.reactstreams.Stream;
@@ -11,6 +24,7 @@ import org.testng.annotations.Test;
 /**
  * TCL test.
  */
+@Test
 public class CountPublisherTest extends PublisherVerification<Integer> {
     /**
      * Constructor.
@@ -45,21 +59,21 @@ public class CountPublisherTest extends PublisherVerification<Integer> {
     public long boundedDepthOfOnNextAndRequestRecursion() {
         return 1;
     }
-    
+
     @Override
     @Test
     public void spec317_mustSupportACumulativePendingElementCountUpToLongMaxValue() throws Throwable {
-        
+
     }
-    
+
     @Override
     @Test
     public void spec317_mustSupportAPendingElementCountUpToLongMaxValue() throws Throwable {
     }
-    
+
     @Override
     @Test
     public void spec317_mustSignalOnErrorWhenPendingAboveLongMaxValue() throws Throwable {
-        
+
     }
 }
